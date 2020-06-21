bcf6c27814d2bc7c08fd3dfa22ef341ce456b637
Verrazano <hdmscratched@gmail.com>
2020-06-21 10:51:44 -0500

    added is to the operators

--- docs/prism-as.js
+++ docs/prism-as.js
@@ -1,7 +1,7 @@
 Prism.languages.as = Prism.languages.extend('c', {
 	'keyword': /\b(?:Animation|APIPlayer|APIServer|AttachmentPoint|BrainVars|C[A-Z]+[a-zA-Z]+|dictionary|Driver|HeadsPack|HitInfo|ImageData|MovementVars|Noise|PixelOffset|PlannerState|Random|SColor|ScriptData|Sector|ShapeConsts|ShapePlatformDirection|ShapeVars|SMaterial|SMesh|SpriteConsts|SpriteVars|string|TeamTexture|Tile|Vec2f|Vertex|f32|u8|u16|u32|s8|s16|s32|bool|break|case|catch|char|char16_t|char32_t|class|shared|const|constexpr|const_cast|continue|decltype|default|delete|do|double|dynamic_cast|else|enum|explicit|export|extern|float|for|friend|goto|if|inline|int|int8_t|int16_t|int32_t|int64_t|uint8_t|uint16_t|uint32_t|uint64_t|long|mutable|namespace|new|noexcept|nullptr|operator|private|protected|public|register|reinterpret_cast|return|short|signed|sizeof|static|static_assert|static_cast|struct|switch|template|this|thread_local|throw|try|typedef|typeid|typename|union|unsigned|using|virtual|void|volatile|wchar_t|while)\b/,
 	'boolean': /\b(?:true|false)\b/,
-	'operator': /--?|\+\+?|!=?|<{1,2}=?|>{1,2}=?|->|:{1,2}|={1,2}|\^|~|%|&{1,2}|@|\|\|?|\?|\*|\/|\b(?:and|and_eq|bitand|bitor|not|not_eq|or|or_eq|xor|xor_eq)\b/
+	'operator': /--?|\+\+?|!=?|<{1,2}=?|>{1,2}=?|->|:{1,2}|={1,2}|\^|~|%|&{1,2}|@|\|\|?|\?|\*|\/|\b(?:and|and_eq|bitand|bitor|not|not_eq|or|or_eq|xor|xor_eq|is)\b/
 });
 
 Prism.languages.insertBefore('as', 'keyword', {
